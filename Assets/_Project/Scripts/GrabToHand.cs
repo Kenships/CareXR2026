bf5680d6cb09c6dbcb15ded3de09f34219d1b195
Kenneth <kenships0402@gmail.com>
2026-03-20 23:23:52 -0400

    stars can pick up

---
+++ Assets/_Project/Scripts/GrabToHand.cs
@@ -0,0 +1,14 @@
+using System;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+
+[RequireComponent(typeof(XRGrabInteractable))]
+public class GrabToHand : MonoBehaviour
+{
+    private XRGrabInteractable _grabInteractable;
+
+    private void Start()
+    {
+        
+    }
+}
